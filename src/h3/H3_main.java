9b82d363f79cd1d3e2c760f7a0c2fcbd8cc91082
maximilian107 <maximilian.s107@evt.schule.koeln>
2026-01-10 12:48:51 +0100

    Add H3 (complete)

--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -2,14 +2,16 @@
 
 package h3;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class H3_main{
     public static void main(String[] args){
-        int[] x = {1,4,3,2};
-        int[] y = {8,5,7,6};
+        int[] x = {1,5,5,2};
+        int[] y = {4,6,3,7};
 
-        System.out.println(Arrays.toString(mergeSort(x,y)));
+        System.out.println("x:\t\t\t\t"+Arrays.toString(x)+"\ny:\t\t\t\t"+Arrays.toString(y)+"\nmergeSort(x,y): "+Arrays.toString(mergeSort(x,y)));
     }
 
     public static int[] mergeSort(int[] x,int[] y){
@@ -20,10 +22,37 @@ public class H3_main{
         for (int b=0;b<y.length;b++){
             merged[x.length+b]=y[b];
         }
-        int[] sorted = new int[merged.length];
-        for (int c=0;c<sorted.length;c++){
 
+        int[] sorted = new int[merged.length];
+        for (int c=0;c<merged.length;c++){
+            sorted[c]=Integer.valueOf(merged[c]);
         }
-        return merged;
+        Arrays.sort(sorted);
+
+        int[] finished=new int[sorted.length];
+        int minus=0;
+        for (int d=0;d<finished.length;d++){
+            finished[d]=Integer.MIN_VALUE;
+        }
+        for (int e=0;e<finished.length;e++){
+            if (e==0){
+                minus+=0;
+            }else if (sorted[e-1] == sorted[e]){
+                minus++;
+            }
+            finished[e-minus]=Integer.valueOf(sorted[e]);
+        }
+        int minValues=0;
+        for (int f=0;f<finished.length;f++){
+            if (finished[f]==Integer.MIN_VALUE){
+                minValues++;
+            }
+        }
+
+        int[] shorten=new int[finished.length-minValues];
+        for (int g=0;g<shorten.length;g++){
+            shorten[g]=finished[g];
+        }
+        return shorten;
     }
 }
